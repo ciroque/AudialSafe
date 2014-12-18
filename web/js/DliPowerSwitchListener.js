8ee6f1cbdbad3a1d1588f57f522f4bc5a7c0618d
Steve Wagner <swagner@marchex.com>
2014-12-18 09:35:59 -0800

    Now making the AJAX call to set the outlet state on the DLI switch.

--- web/js/DliPowerSwitchListener.js
+++ web/js/DliPowerSwitchListener.js
@@ -5,7 +5,7 @@ com.marchex = com.marchex || {};
 com.marchex.audial = com.marchex.audial || {};
 
 com.marchex.audial.DliPowerSwitchListener = function(eventManager, logger, opts) {
-    this.opts = opts || { switchAddress: '192.168.1.1', firstLightOutlet: 1, secondLightOutlet: 2 };
+    this.opts = opts || { switchAddress: '127.0.0.1:80', firstLightOutlet: 1, secondLightOutlet: 2 };
     this.eventManager = eventManager;
     this.logger = logger;
     this.primarySet = false;
@@ -65,7 +65,22 @@ com.marchex.audial.DliPowerSwitchListener.prototype.sendControlMessageToSwitch =
     this.logger.write('DliPowerSwitchListener::sendControlMessageToSwitch');
     // TODO: Make Ajax call...
 
-    console.log('Yep, I sent the message to the power switch: outlet(' + outlet + '), state(' + state + ')');
+    var self = this;
+    var url = this.opts.switchAddress + '/outlet?' + outlet + '=' + (state ? 'ON' : 'OFF');
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'application/json; charset=utf-8',
+        username: 'admin',
+        password: '1234',
+        processData: false,
+        contentType: 'application/json',
+        success: function() { self.logger.write('Called the DLI Logger successfully! outlet(' + outlet + '), state(' + state + ')'); },
+        error: function(xhr, ajaxOptions, error) { self.logger.write('ERROR Calling the DLI Logger! [' + error + '] outlet(' + outlet + '), state(' + state + ')'); }
+    });
+
+    this.logger.write('USED URL: ' + url);
 
     return this;
 };
