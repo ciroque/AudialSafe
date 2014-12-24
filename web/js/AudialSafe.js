5764db30147e2067342849a12404e9ae7ceec646
Steve Wagner <swagner@marchex.com>
2014-12-24 14:22:28 -0800

    renaming

--- web/js/AudialSafe.js
+++ web/js/AudialSafe.js
@@ -4,26 +4,29 @@ var org = org || {};
 org.ciroque = org.ciroque || {};
 org.ciroque.audial = org.ciroque.audial || {};
 
-org.ciroque.audial.AudialSafe = function() {
+var ns = org.ciroque.audial;
+
+ns.AudialSafe = function() {
 
     var loggerOptions = { enabled: true };
+    var ns = org.ciroque.audial;
 
-    this.logger = new org.ciroque.audial.Logger(loggerOptions);
-    this.eventManager = new org.ciroque.audial.EventManager(this.logger);
-    this.storage = new org.ciroque.audial.Storage(this.logger);
-    this.settingSliders = new org.ciroque.audial.SettingSliders(this.eventManager, this.logger, this.storage);
-    this.advancedSettings = new org.ciroque.audial.AdvancedSettings(this.eventManager, this.logger, this.storage);
-    this.masterControls = new org.ciroque.audial.MasterControls(this.eventManager, this.logger);
-    this.levelMeter = new org.ciroque.audial.LevelMeter(this.eventManager, this.logger);
-    this.audio = new org.ciroque.audial.Audio(this.eventManager, this.logger);
-    this.thresholdListener = new org.ciroque.audial.ThresholdMonitor(this.eventManager, this.logger);
-    this.uiWarningListener = new org.ciroque.audial.UiWarningListener(this.eventManager, this.logger);
-    this.dlpSwitchListener = new org.ciroque.audial.DliPowerSwitchListener(this.eventManager, this.logger);
+    this.logger = new ns.Logger(loggerOptions);
+    this.eventManager = new ns.EventManager(this.logger);
+    this.storage = new ns.Storage(this.logger);
+    this.settingSliders = new ns.SettingSliders(this.eventManager, this.logger, this.storage);
+    this.advancedSettings = new ns.AdvancedSettings(this.eventManager, this.logger, this.storage);
+    this.masterControls = new ns.MasterControls(this.eventManager, this.logger);
+    this.levelMeter = new ns.LevelMeter(this.eventManager, this.logger);
+    this.audio = new ns.Audio(this.eventManager, this.logger);
+    this.thresholdListener = new ns.ThresholdMonitor(this.eventManager, this.logger);
+    this.uiWarningListener = new ns.UiWarningListener(this.eventManager, this.logger);
+    this.dlpSwitchListener = new ns.DliPowerSwitchListener(this.eventManager, this.logger);
 
     return this;
 };
 
-org.ciroque.audial.AudialSafe.prototype.init = function() {
+ns.AudialSafe.prototype.init = function() {
     this.logger.write('AudialSafe::init');
 
     this.settingSliders.init();
